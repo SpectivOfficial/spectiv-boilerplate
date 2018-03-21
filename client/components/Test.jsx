020f6fed20ba75b6052ca1c44dc89c1d96e0e4dd
Won Jin Chung <chungw51993@gmail.com>
2018-03-21 11:47:49 -0500

    Modified README and minor changes

--- client/components/Test.jsx
+++ client/components/Test.jsx
@@ -6,7 +6,7 @@ class Test extends Component {
   render() {
     return (
       <div>
-        <h1>React Boilerplate is ALIVE</h1>
+        <h1>Spectiv Boilerplate is ALIVE</h1>
       </div>
     );
   }
